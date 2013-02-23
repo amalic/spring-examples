421474e21c8fca7269eb9b4a9e3d1c53090478ab
amalic <alexander.malic@gmail.com>
2013-02-23 03:30:15 +0100

    Initial commit

---
+++ spring_json/webapp_spring_json/src/main/java/org/amalic/controller/JSONController.java
@@ -0,0 +1,31 @@
+package org.amalic.controller;
+
+import org.amalic.model.TestModel;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/jsons")
+public class JSONController {
+	
+	private Log log = LogFactory.getLog(JSONController.class);
+
+	@RequestMapping(value="/getSomethingAsJSON/{property}/{property2}", method = RequestMethod.GET)
+	public @ResponseBody TestModel getSomethingAsJSON(@PathVariable String property, @PathVariable String property2) {
+		log.info("JUHU, RequestMapping funktioniert :)");
+		
+		TestModel testModel = new TestModel();
+		testModel.setProperty(property);
+		testModel.setSomeOtherProperty(property2);
+		testModel.setStringArray(new String[]{"string1", "string2"});
+		
+		return testModel;
+	}
+	
+}
---
+++ spring_json/webapp_spring_json/src/main/java/org/amalic/model/TestModel.java
@@ -0,0 +1,31 @@
+package org.amalic.model;
+
+public class TestModel {
+	String property;
+	String someOtherProperty;
+	String stringArray[];
+	
+	public String getProperty() {
+		return property;
+	}
+	
+	public void setProperty(String property) {
+		this.property = property;
+	}
+
+	public String[] getStringArray() {
+		return stringArray;
+	}
+	
+	public void setStringArray(String[] stringArray) {
+		this.stringArray = stringArray;
+	}
+
+	public String getSomeOtherProperty() {
+		return someOtherProperty;
+	}
+	public void setSomeOtherProperty(String someOtherProperty) {
+		this.someOtherProperty = someOtherProperty;
+	}
+	
+}
