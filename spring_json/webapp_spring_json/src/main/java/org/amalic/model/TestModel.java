766e8037deb0516cc55a0cbe500950c96af96f25
amalic <alexander.malic@gmail.com>
2013-02-24 14:05:29 +0100

    added lombok

--- spring_json/webapp_spring_json/src/main/java/org/amalic/model/TestModel.java
+++ spring_json/webapp_spring_json/src/main/java/org/amalic/model/TestModel.java
@@ -1,31 +1,10 @@
 package org.amalic.model;
 
+import lombok.Data;
+
+@Data
 public class TestModel {
 	String property;
 	String someOtherProperty;
 	String stringArray[];
-	
-	public String getProperty() {
-		return property;
-	}
-	
-	public void setProperty(String property) {
-		this.property = property;
-	}
-
-	public String[] getStringArray() {
-		return stringArray;
-	}
-	
-	public void setStringArray(String[] stringArray) {
-		this.stringArray = stringArray;
-	}
-
-	public String getSomeOtherProperty() {
-		return someOtherProperty;
-	}
-	public void setSomeOtherProperty(String someOtherProperty) {
-		this.someOtherProperty = someOtherProperty;
-	}
-	
 }
